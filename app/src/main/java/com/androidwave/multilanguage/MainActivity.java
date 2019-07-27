b70694d1e85c034ca1e4e980129ea02b263de2ea
Surya <surya.prakash@aurigait.com>
2019-07-28 00:15:20 +0530

    reformat file

--- app/src/main/java/com/androidwave/multilanguage/MainActivity.java
+++ app/src/main/java/com/androidwave/multilanguage/MainActivity.java
@@ -13,99 +13,97 @@ import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends BaseActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+    implements NavigationView.OnNavigationItemSelectedListener {
+  DrawerLayout drawerLayout;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    FloatingActionButton fab = findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show();
+      }
+    });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+    drawerLayout = findViewById(R.id.drawer_layout);
+    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+        R.string.navigation_drawer_close);
+    drawerLayout.addDrawerListener(toggle);
+    toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+    NavigationView navigationView = findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
+  }
+
+  @Override
+  public void onBackPressed() {
+    if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+      drawerLayout.closeDrawer(GravityCompat.START);
+    } else {
+      super.onBackPressed();
     }
+  }
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    switch (id) {
+      case R.id.local_english:
+        LocaleManager.setNewLocale(this, LocaleManager.LANGUAGE_KEY_ENGLISH);
+        recreate();
+        return true;
+      case R.id.local_hindi:
+        LocaleManager.setNewLocale(this, LocaleManager.LANGUAGE_KEY_HINDI);
+        recreate();
+        return true;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+      case R.id.local_spanish:
+        LocaleManager.setNewLocale(this, LocaleManager.LANGUAGE_KEY_SPANISH);
+        recreate();
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.local_english:
-                LocaleManager.setNewLocale(this, LocaleManager.LANGUAGE_KEY_ENGLISH);
-                recreate();
-                return true;
-            case R.id.local_hindi:
-                LocaleManager.setNewLocale(this, LocaleManager.LANGUAGE_KEY_HINDI);
-                recreate();
-                return true;
+    return super.onOptionsItemSelected(item);
+  }
 
-            case R.id.local_spanish:
-                LocaleManager.setNewLocale(this, LocaleManager.LANGUAGE_KEY_SPANISH);
-                recreate();
-                return true;
-        }
+  @SuppressWarnings("StatementWithEmptyBody")
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item) {
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+    switch (id) {
+      case R.id.nav_camera:
+        break;
+      case R.id.nav_gallery:
+        break;
+      case R.id.nav_slideshow:
+        break;
+      case R.id.nav_manage:
+        break;
+      case R.id.nav_share:
+        break;
+      case R.id.nav_send:
+        break;
 
-        return super.onOptionsItemSelected(item);
+      default:
+        break;
     }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.nav_camera:
-                break;
-            case R.id.nav_gallery:
-                break;
-            case R.id.nav_slideshow:
-                break;
-            case R.id.nav_manage:
-                break;
-            case R.id.nav_share:
-                break;
-            case R.id.nav_send:
-                break;
-
-            default:
-                break;
-        }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
+    drawerLayout.closeDrawer(GravityCompat.START);
+    return true;
+  }
 }
